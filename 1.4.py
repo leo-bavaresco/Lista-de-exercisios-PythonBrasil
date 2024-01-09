ef7b8b0ff48b40812085d4b1b50aec7a6a62fb61
Leonardo Guido Bavaresco <123987951+leo-bavaresco@users.noreply.github.com>
2024-01-09 16:18:34 -0300

    1.4.py

--- 1.4.py
+++ 1.4.py
@@ -1,3 +1,5 @@
+#1.4 Faça um Programa que peça as 4 notas bimestrais e mostre a média.
+
 nota1 = float(input('Digite a primeira nota: '))
 nota2 = float(input('Digite a primeira nota: '))
 nota3 = float(input('Digite a primeira nota: '))
