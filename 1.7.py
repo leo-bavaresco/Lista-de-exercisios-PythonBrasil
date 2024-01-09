90817716d1f4550d86c9c58a4ffccb9ca5b5b464
Leonardo Guido Bavaresco <123987951+leo-bavaresco@users.noreply.github.com>
2024-01-09 18:21:09 -0300

    Add files via upload

---
+++ 1.7.py
@@ -0,0 +1,5 @@
+base = float(input('Digite o comprimento da base do quadrado: '))
+
+area_quadrado = base ** 2
+
+print(f"A área do quadrado é {area_quadrado:} m²")
---
+++ 1.9.py
@@ -0,0 +1,5 @@
+graus_f = float(input('Digite a temperatura em Fahrenheit: '))
+
+C = 5 * ((graus_f-32) / 9)
+
+print (f'A temperatura em celsius é {C: .2f}°')
