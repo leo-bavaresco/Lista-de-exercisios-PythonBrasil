b23fbf9985a792dd3f39f7be167b1dba45ef4dcf
Leonardo Guido Bavaresco <123987951+leo-bavaresco@users.noreply.github.com>
2024-01-09 15:45:07 -0300

    Add files via upload

---
+++ 1.2.py
@@ -0,0 +1,3 @@
+numero = input ('Digite um numero: ')
+
+print ('Onumero informado é ',numero)
---
+++ 1.3.py
@@ -0,0 +1,4 @@
+numero1 = float (input("Digite um numero: "))
+numero2 = float (input('digite mais um numero: '))
+
+print ('A soma dos numeros digitados é:', numero1+numero2)
