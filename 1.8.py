a50e836dc1be016e3d0b8b9d514696bc0773517d
Leonardo Guido Bavaresco <123987951+leo-bavaresco@users.noreply.github.com>
2024-01-17 10:31:05 -0300

    Delete 1.8.py

--- 1.8.py
+++
@@ -1,6 +0,0 @@
-salariohora = float(input('Digite o salario por hora: '))
-horatrabalhada = float(input('Digite a quantidade de horas trabalhadas no mes: '))
-
-salariomes = salariohora * horatrabalhada
-
-print(f"O salario desse mes é {salariomes:} reais")
