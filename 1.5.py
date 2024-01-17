157e06b469c186b913466f29c1738b598bcb7fe6
Leonardo Guido Bavaresco <123987951+leo-bavaresco@users.noreply.github.com>
2024-01-17 10:46:55 -0300

    Delete 1.5.py

--- 1.5.py
+++
@@ -1,6 +0,0 @@
-#1.5 Faça um Programa que converta metros para centímetros.
-
-metros = float(input('Digite a quantide de metro: '))
-centimetros = metros * 100
-
-print (metros,'metros equivalem a', centimetros,'centimetros')
