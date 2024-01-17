eb8e86d12bb537faad5839d24d91eec31179109e
Leonardo Guido Bavaresco <123987951+leo-bavaresco@users.noreply.github.com>
2024-01-17 10:31:59 -0300

    Delete 1.13.py

--- 1.13.py
+++
@@ -1,9 +0,0 @@
-opcao = input("Por favor, informe o sexo (digite 'homem' ou 'mulher'): ")
-altura = float(input('Informe sua altura: '))
-
-if opcao.lower() == 'homem':
-    peso_ideal = (72.7*altura) - 58
-else:
-    peso_ideal = (62.1*altura) - 44.7
-
-print(f"Seu peso ideal é: {peso_ideal:.2f} kg")
