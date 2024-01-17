6c58088cafab904b30488b2f1a7c860ef6694226
Leonardo Guido Bavaresco <123987951+leo-bavaresco@users.noreply.github.com>
2024-01-17 15:14:30 -0300

    Add files via upload

---
+++ Estrutura-De-Decisao/1.py
@@ -0,0 +1,9 @@
+# Solicita dois numeros
+n1 = float(input('Digite um numero aleatorio: '))
+n2 = float(input('Digite outro numero aleatorio: '))
+
+# Verificção de qual numero é maior e retorna o resultado
+if n1 > n2:
+    print ('O primeiro numero é maior')
+else:
+    print ('O segundo numero é maior')
---
+++ Estrutura-De-Decisao/2.py
@@ -0,0 +1,8 @@
+# Solicita o um numero
+numero = float(input('Digite um valor: '))
+
+# Verifica se o numero é positivo ou negativo e retorna o resultado
+if numero > 0:
+    print (f'O valor informado é positivo: {numero}')
+else:
+    print (f'O valor informado é negativo: {numero}')
